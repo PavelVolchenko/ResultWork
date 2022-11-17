ed6d211197184adb246c95fe64f9ec562a6e70a3
Pavel Volchenko <2793553@gmail.com>
2022-11-18 00:39:53 +0300

    Wrote a program

--- Program.cs
+++ Program.cs
@@ -1,8 +1,59 @@
-﻿/*  Написать программу, которая из имеющегося массива строк формирует
-    массив из строк, длина которых меньше либо равна 3 символа. 
-    Первоначальный массив можно ввести с клавиатуры, либо задать на старте
-    выполнения алгоритма. При решении не рекомендуется пользоваться коллекциями,
-    лучше обойтись исключительно массивами.                                   
-    ["hello", "2", "world", ":-)"] -> ["2", ":-)"]
-    ["1234", "1567", "-2", "computer science"] -> ["-2"]
-    ["Russia", "Denmark", "Kazan"] -> []                                     */
+﻿string[] fillArray(int arraySize)
+{
+    string[] userArray = new string[arraySize];
+    for (int i = 0; i < arraySize; i++)
+    {
+        Console.Write($"Введите {i + 1} строку из {arraySize}: ");
+        userArray[i] = Console.ReadLine();
+    }
+    return userArray;
+}
+
+int calculateFutureArraySize(string[] userArray)
+{
+    int count = 0;
+    for (int i = 0; i < userArray.Length; i++)
+    {
+        if (userArray[i].Length <= 3) count++;
+    }
+    return count;
+}
+
+string[] resultArray(string[] userArray, int sizeForNewArray)
+{
+    string[] resultArray = new string[sizeForNewArray];
+    int j = 0;
+    for (int i = 0; i < userArray.Length; i++)
+    {
+        if (userArray[i].Length <= 3)
+        {
+            resultArray[j] = userArray[i];
+            j++;
+        }
+    }
+    return resultArray;
+}
+
+void printArray(string[] incomingArray)
+{
+    Console.Write("[");
+    for (int i = 0; i < incomingArray.Length; i++)
+    {
+        Console.Write($"\"{incomingArray[i]}\"");
+        if (i < incomingArray.Length -1)
+        {
+            Console.Write(", ");
+        }
+    }
+    Console.WriteLine("]");
+}
+
+Console.Write("Какое кол-во строк будем вносить в массив? ");
+int arraySize = Convert.ToInt32(Console.ReadLine());
+
+string[] userArray = fillArray(arraySize);
+
+int sizeForNewArray = calculateFutureArraySize(userArray);
+
+string[] result = resultArray(userArray, sizeForNewArray);
+printArray(result);
